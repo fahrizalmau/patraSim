99224e1f532f7a1b46477c90f0e31391c569ab56
Fahrizal Maulana <fahrizalmau@gmail.com>
2024-04-30 13:27:21 +0700

    Restructuring struct

--- initNeighborGrid3D.m
+++ initNeighborGrid3D.m
@@ -4,59 +4,59 @@ function G = initNeighborGrid3D(cellsize)
     n = nx*ny*nz;
 
     %preallocation
-    G(n) = struct;
-    
-    
+    G.index = struct('index',    linspace(1, n, n), ...
+                     'initial',  linspace(1,n,n), ...
+                     'isActive', linspace(1,1,n));
+    G.neighbors = struct('ncontact', zeros(1, n), ...
+                         'nsewtb',   zeros(6,n));
+
     idx = 0;
     for k=1:nz
         for j=1:ny
             for i=1:nx
                 idx = idx+1;
-                %active-or-not
-                
-                G(idx).idx = idx;
-                G(idx).newIdx = idx;
-
-                %center
-                G(idx).center = idx;
 
                 %north-south
                 if j==1
-                    G(idx).north = 0;
-                    G(idx).south = idx+nx;
+                    G.neighbors.nsewtb(1,idx) = 0;
+                    G.neighbors.nsewtb(2,idx) = idx+nx;
                 elseif j==ny
-                    G(idx).north = idx-nx;
-                    G(idx).south = 0;
+                    G.neighbors.nsewtb(1,idx) = idx-nx;
+                    G.neighbors.nsewtb(2,idx) = 0;
                 else
-                    G(idx).north = idx-nx;
-                    G(idx).south = idx+nx;
+                    G.neighbors.nsewtb(1,idx) = idx-nx;
+                    G.neighbors.nsewtb(2,idx) = idx+nx;
                 end    
 
                 %west-east
                 if i==1
-                    G(idx).east = idx+1;
-                    G(idx).west = 0;
+                    G.neighbors.nsewtb(3,idx) = idx+1;
+                    G.neighbors.nsewtb(4,idx) = 0;
                 elseif i==nx
-                    G(idx).east = 0;
-                    G(idx).west = idx-1;
+                    G.neighbors.nsewtb(3,idx) = 0;
+                    G.neighbors.nsewtb(4,idx) = idx-1;
                 else
-                    G(idx).east = idx+1;
-                    G(idx).west = idx-1;
+                    G.neighbors.nsewtb(3,idx) = idx+1;
+                    G.neighbors.nsewtb(4,idx) = idx-1;
                 end
 
                 %top-bottom
                 if k==nz
-                    G(idx).top = 0;
-                    G(idx).bottom = idx-nx*ny;
+                    G.neighbors.nsewtb(5,idx) = 0;
+                    G.neighbors.nsewtb(6,idx) = idx-nx*ny;
                 elseif k==1
-                    G(idx).top = idx+nx*ny;
-                    G(idx).bottom = 0;
+                    G.neighbors.nsewtb(5,idx) = idx+nx*ny;
+                    G.neighbors.nsewtb(6,idx) = 0;
                 else
-                    G(idx).top = idx+nx*ny;
-                    G(idx).bottom = idx-nx*ny;
+                    G.neighbors.nsewtb(5,idx) = idx+nx*ny;
+                    G.neighbors.nsewtb(6,idx) = idx-nx*ny;
                 end
             end
         end
     end
+    
+    %assign ncontact
+    G.neighbors.ncontact = sum(G.neighbors.nsewtb~=0, 1);
+
 end
 
